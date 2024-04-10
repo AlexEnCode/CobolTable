43380f45970b5c0a9c441ee282d1058f29809369
Alexandre <146923992+AlexEnCode@users.noreply.github.com>
2024-04-10 16:49:59 +0200

    Update runtable.cbl

--- runtable.cbl
+++ runtable.cbl
@@ -3,7 +3,8 @@
 
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-       
+
+      *Information a entrer dans le tableau
        01  DATAINFO.
            03 FILLER   PIC X(30) VALUE "0001HOKKAI     TARO       0400".
            03 FILLER   PIC X(30) VALUE "0002AOMORI     JIRO       0350".
@@ -15,6 +16,7 @@
            03 FILLER   PIC X(30) VALUE "0008IBARAKI    HACHIRO    1050".
            03 FILLER   PIC X(30) VALUE "0009GUMMA      KURO       0200".
            03 FILLER   PIC X(30) VALUE "0010SAITAMA    JURO       0350".
+      *Redifines fait entrer les valeurs dans la table
        01  DATATABLE REDEFINES DATAINFO.
            02 JAPON          OCCURS 10 TIMES.
             03 WS-ID          PIC A(4)  VALUE SPACE.
@@ -24,7 +26,8 @@
        01  WS-INDEX           PIC 9(2)  VALUE 0.
 
        PROCEDURE DIVISION.
-       
+
+      *Affichage des lignes 2, 5 et 10 
            DISPLAY "------------------------------------".
            DISPLAY "|" WS-ID(2) "|" 
            SPACE WS-LASTNAME(2) "|" 
